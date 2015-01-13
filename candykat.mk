e0d517d83b6c2f31803b81c097b14f1f5af188ed
Alx31 <alex31.couderc@gmail.com>
2015-01-13 22:37:58 +0100

    Validus-ify

--- candykat.mk
+++ candykat.mk
@@ -5,14 +5,14 @@ TARGET_SCREEN_WIDTH := 1920
 # Inherit device configuration
 $(call inherit-product, device/sony/pollux/full_pollux.mk)
 
-# Inherit some common CandyKat stuff.
-$(call inherit-product, vendor/candykat/config/gsm.mk)
+# Inherit some common validus stuff.
+$(call inherit-product, vendor/validus/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/candykat/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit some common Phone stuff.
-$(call inherit-product, vendor/candykat/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
@@ -21,7 +21,7 @@ PRODUCT_RELEASE_NAME := pollux
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pollux
-PRODUCT_NAME := candykat_pollux
+PRODUCT_NAME := validus_pollux
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := SGP32x
 PRODUCT_MANUFACTURER := Sony
